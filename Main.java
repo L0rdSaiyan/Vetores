67ec95aff20bf336cf16be38e6ff7c21d794d92a
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-01-19 11:52:53 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*
+  Exercício de vetores 02
+*/
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		//Se você já souber cada valor de cada índice do vetor, utilizar desta maneira:
+		int vetor[] = {1,2,3,4,5,6,7,8,9,10};
+		
+		//Estrutura de repetição para a exibição do valor do vetor de acordo com a posição de uma variável de repetição utilizando "[]".
+	   for(int repetition=0; repetition<10; repetition++){
+	       
+	       //Exibir o valor de Vetor de acordo com a posição de repetition
+	       System.out.println("O vetor "+vetor[repetition]+" está na posição "+repetition);
+	       
+	   }
+	
+	}
+}
