2f84972ce68c8e6f5f7561182d2ebe1d117e6bfd
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-01-19 11:54:18 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
 /*
-  Exercício de vetores 02
+  Exercício de vetores 01
 */
 
 public class Main
 {
 	public static void main(String[] args) {
 		
-		//Se você já souber cada valor de cada índice do vetor, utilizar desta maneira:
-		int vetor[] = {1,2,3,4,5,6,7,8,9,10};
+		//os "[]" podem ser colocados tanto do lado direito como do lado esquerdo do objeto.
+		int vetor[] = new int [10]; //O vetor tem o índice 10.
+		
+		//Aplicando valores aos índices.
+		vetor[0] = 1;
+		vetor[1] = 2;
+		vetor[2] = 3;
+		vetor[3] = 4;
+		vetor[4] = 5;
+		vetor[5] = 6;
+		vetor[6] = 7;
+		vetor[7] = 8;
+		vetor[8] = 9;
+		vetor[9] = 10;
 		
 		//Estrutura de repetição para a exibição do valor do vetor de acordo com a posição de uma variável de repetição utilizando "[]".
 	   for(int repetition=0; repetition<10; repetition++){
